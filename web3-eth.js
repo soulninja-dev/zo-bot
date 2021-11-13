0f66cf812ba08f65c207b566a6b842a59304e68b
Abhinav Marwaha <abhinavmarwaha.bt19cse@pec.edu.in>
2021-11-13 11:25:36 +0530

    web3 ens reolution

--- web3-eth.js
+++ web3-eth.js
@@ -1,10 +1,10 @@
-const Web3 = require('web3');
+const Web3 = require("web3");
 
-var provider = 'ADD_YOUR_ETHEREUM_NODE_URL';
+var provider = "ADD_YOUR_ETHEREUM_NODE_URL";
 var web3Provider = new Web3.providers.HttpProvider(provider);
 var web3 = new Web3(web3Provider);
 web3.eth.getBlockNumber().then((result) => {
-console.log("Latest Ethereum Block is ",result);
+  console.log("Latest Ethereum Block is ", result);
 });
 
 // import TwitterApi from 'twitter-api-v2';
@@ -12,62 +12,80 @@ console.log("Latest Ethereum Block is ",result);
 // const roClient = twitterClient.readOnly;
 // const user = await roClient.v2.userByUsername('The_Zo_World');
 
-const config = require('./config');
-const twitter = require('twitter-lite');
+const config = require("./config");
+const twitter = require("twitter-lite");
 const client = new twitter(config);
 
-function sendTxn(toAddr, tweet){
-  web3.eth.sendTransaction({from: '0x123...', data: toAddr})
-  .once('sending', function(payload){  })
-  .once('sent', function(payload){  })
-  .once('transactionHash', function(hash){ })
-  .once('receipt', function(receipt){  })
-  .on('confirmation', function(confNumber, receipt, latestBlockHash){ })
-  .on('error', function(error){  })
-  .then(function(receipt){
-    let statusObj = {
-        status: "Hi @" + tweet.user.screen_name + "we sent you an NFT, txn id: " + receipt.txn,
+function sendTxn(toAddr, tweet) {
+  web3.eth
+    .sendTransaction({ from: "0x123...", data: toAddr })
+    .once("sending", function (payload) {})
+    .once("sent", function (payload) {})
+    .once("transactionHash", function (hash) {})
+    .once("receipt", function (receipt) {})
+    .on("confirmation", function (confNumber, receipt, latestBlockHash) {})
+    .on("error", function (error) {})
+    .then(function (receipt) {
+      let statusObj = {
+        status:
+          "Hi @" +
+          tweet.user.screen_name +
+          "we sent you an NFT, txn id: " +
+          receipt.txn,
         in_reply_to_status_id: tweet.id_str,
-    }
-    client.post('statuses/update', statusObj).then(result => {
-      console.log('You successfully tweeted this : "' + result.text + '"');
-    }).catch(console.error);
-
-  });
+      };
+      client
+        .post("statuses/update", statusObj)
+        .then((result) => {
+          console.log('You successfully tweeted this : "' + result.text + '"');
+        })
+        .catch(console.error);
+    });
 }
 
-function getAddressAndSend(msg){
+function getAddressAndSend(msg) {
   let start = msg.indexOf("0x");
-  let end = msg.indexOf(" ", start+1);
+  let end = msg.indexOf(" ", start + 1);
   let addr = msg.substring(start, end);
   let verified = isAddress(addr);
-  if(verified){
-    sendTxn(addr, msg)
-  }
-  else{
-    console.log("Invalid address")
+  if (verified) {
+    sendTxn(addr, msg);
+  } else {
+    web3.eth.ens.getAddress(addr).then(function (address) {
+      if (isAddress(address)) {
+        sendTxn(addr, msg); 
+      } else console.log("Invalid address");
+    });
   }
 }
 
 var isAddress = function (address) {
   if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
-      return false;
-  } else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
-      return true;
+    return false;
+  } else if (
+    /^(0x)?[0-9a-f]{40}$/.test(address) ||
+    /^(0x)?[0-9A-F]{40}$/.test(address)
+  ) {
+    return true;
   } else {
-      return isChecksumAddress(address);
+    return isChecksumAddress(address);
   }
 };
 
 var isChecksumAddress = function (address) {
-  address = address.replace('0x','');
+  address = address.replace("0x", "");
   var addressHash = sha3(address.toLowerCase());
-  for (var i = 0; i < 40; i++ ) {
-      if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) || (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
-          return false;
-      }
+  for (var i = 0; i < 40; i++) {
+    if (
+      (parseInt(addressHash[i], 16) > 7 &&
+        address[i].toUpperCase() !== address[i]) ||
+      (parseInt(addressHash[i], 16) <= 7 &&
+        address[i].toLowerCase() !== address[i])
+    ) {
+      return false;
+    }
   }
   return true;
 };
 
-module.exports = {getAddressAndSend};
+module.exports = { getAddressAndSend };

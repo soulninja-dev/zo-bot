2695150aa17034a26007d4d5b4ae8d285db7bafa
Abhinav Marwaha <abhinavmarwaha.bt19cse@pec.edu.in>
2021-11-20 07:51:54 +0530

    send-dm only sends to admin... why??

--- app/send-dm/command.js
+++ app/send-dm/command.js
@@ -12,29 +12,25 @@ const sendDmCommand = (message, args) => {
 
   if (!role) return message.reply("There is not such a role!");
 
-  console.log("messages sent to: ");
+  process.stdout.write("messages sent to: ");
 
-  for (const member of message.guild.members.cache.values()) {
-    console.log(member.user.username + ", ");
+  for (const member of role.members.values()) {
+    let sendString = message.content.substr(_2space);
+    let replaceArray = ["username"],
+      replaceWith = [member.user.username];
 
-    for (const memberRole of member.roles.cache.values()) {
-      if (memberRole.id === role.id) {
-        let sendString = message.content.substr(_2space);
-        let replaceArray = ["username"],
-          replaceWith = [member.user.username];
-
-        for (let i = 0; i < replaceArray.length; i++) {
-          sendString = sendString.replace(
-            new RegExp("{" + replaceArray[i] + "}", "gi"),
-            replaceWith[i]
-          );
-        }
-
-        member.user.send(sendString);
-      }
+    for (let i = 0; i < replaceArray.length; i++) {
+      sendString = sendString.replace(
+        new RegExp("{" + replaceArray[i] + "}", "gi"),
+        replaceWith[i]
+      );
     }
+
+    member.user.send(sendString);
+    process.stdout.write(member.user.username + ", ");
   }
   message.reply("messages sent");
+  console.log("");
 };
 
 module.exports = { sendDmCommand };
